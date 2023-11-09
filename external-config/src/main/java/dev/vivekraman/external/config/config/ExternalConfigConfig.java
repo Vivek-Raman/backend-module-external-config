c8b0f450768c1bf66f1ab6ff294d261f7b400b7d
Vivek Raman <vr.ac4bf@live.com>
2023-11-09 15:53:33 +0530

    Cleanup

--- external-config/src/main/java/dev/vivekraman/external/config/config/ExternalConfigConfig.java
+++ external-config/src/main/java/dev/vivekraman/external/config/config/ExternalConfigConfig.java
@@ -3,7 +3,6 @@ package dev.vivekraman.external.config.config;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 public class ExternalConfigConfig {
